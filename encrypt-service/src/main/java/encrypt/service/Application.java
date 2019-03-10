e13f6fbaf752531a07d7f07b25d719e440e3e5f5
Iván López <lopez.ivan@gmail.com>
2019-03-10 12:57:06 +0100

    Encrypt-service initial commit

---
+++ encrypt-service/src/main/java/encrypt/service/Application.java
@@ -0,0 +1,10 @@
+package encrypt.service;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
